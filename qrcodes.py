07892ca5373802640385c852f88449d24ca799b8
Elijah Goris <coeliman24@gmail.com>
2024-10-11 09:43:54 -0600

    Successfully tracks aruco codes via ID

--- qrcodes.py
+++ qrcodes.py
@@ -2,17 +2,34 @@ import numpy as np
 import cv2
 aruco_type = cv2.aruco.DICT_4X4_1000
 arucoid = 1
-
+cam = cv2.VideoCapture(0)
+var = False
 arucoDict = cv2.aruco.getPredefinedDictionary(aruco_type)
-
 tag_size  = 1000
 tag = np.zeros((tag_size, tag_size,1), dtype=np.uint8)
 cv2.aruco.generateImageMarker(arucoDict,arucoid,tag_size,tag,1)
-
 tag_name = f"arucoMarkers/" + str(aruco_type) + f"_" + str(arucoid) + f".png"
-
 cv2.imwrite(tag_name,tag)
-cv2.imshow("Aruco",tag)
+#aruco,tag is the aruco tag
 
+def FindAruco(imag,markerSize=4,total_markers=1000,draw=True):
+    gray=cv2.cvtColor(imag,cv2.COLOR_BGR2GRAY)
+    key = getattr(cv2.aruco,f'DICT_{markerSize}X{markerSize}_{total_markers}')
+    arucoDict= cv2.aruco.getPredefinedDictionary(key)
+    arucoParam = cv2.aruco.DetectorParameters()
+    bbox,ids,_=cv2.aruco.detectMarkers(gray,arucoDict,parameters=arucoParam)
+    print(ids)
+
+
+while True:
+    if var == True:
+      img=tag
+      img = cv2.resize(img,(0,0),fx=0.7,fy=0.7)
+    else:
+        _,img=cam.read()
+    FindAruco(img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    cv2.imshow("Camera",img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
