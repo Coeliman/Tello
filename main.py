e4792d54d2d6c990759240b5b59c8c33e3b37421
Elijah Goris <coeliman24@gmail.com>
2025-01-23 08:58:44 -0700

    Merged facerecog with main

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import numpy as np
 import cv2
 from cv2 import aruco
-import djitellopy as tello
 import asyncio
+import djitellopy as tello
+
 
 tello = tello.Tello()
 tello.connect()
@@ -13,7 +14,8 @@ arucoid = 1
 useDrone = True
 loopcount = 0
 move = 0
-
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+track = 0
 if useDrone:
     pass
 else:
@@ -49,22 +51,24 @@ def EXIT():
     tello.land()
     cv2.destroyAllWindows()
 def ScreenSplitLines(imag):
+    global ScreenX_Half, ScreenY_Half
     size_info = imag.shape
-    #print(size_info)
-    #maybe Y,X? Don't know, figure out exactly what its passing
+    # print(size_info)
+    # maybe Y,X? Don't know, figure out exactly what its passing
     ScreenX = int(size_info[1])
     ScreenY = int(size_info[0])
     ScreenX_Half = int(ScreenX / 2)
     ScreenY_Half = int(ScreenY / 2)
-    #print(f"X End: {ScreenX} X Half: {ScreenX_Half} Y End: {ScreenY} Y Half: {ScreenY_Half}")
+    # print(f"X End: {ScreenX} X Half: {ScreenX_Half} Y End: {ScreenY} Y Half: {ScreenY_Half}")
     cv2.line(img, (ScreenX_Half, 0), (ScreenX_Half, ScreenY), (0, 255, 0), 5)
     cv2.line(img, (0, ScreenY_Half), (ScreenX, ScreenY_Half), (255, 0, 0), 5)
-    #creates thresholds for drone turning
-    global thresholdRX,thresholdLX,thresholdUY,thresholdBY
-    thresholdRX = ScreenX_Half + 50
-    thresholdLX = ScreenX_Half - 50
-    thresholdUY = ScreenY_Half + 50
-    thresholdBY = ScreenY_Half - 50
+    # creates thresholds for drone turning
+    global thresholdRX, thresholdLX, thresholdUY, thresholdBY
+    thresholdRX = ScreenX_Half + ScreenX * 0.2
+    thresholdLX = ScreenX_Half - ScreenX * 0.2
+    thresholdUY = ScreenY_Half - ScreenY * 0.1
+
+    thresholdBY = ScreenY_Half + ScreenY * 0.1
 def CoordMath(bbox):
     global x
     global y
@@ -78,29 +82,51 @@ def DrawAruco(cam,crn,id):
     ScreenSplitLines(cam)
     aruco.drawDetectedMarkers(cam,crn,id)
 def DroneController():
-    if x and y == int:
+    global xm, ym, track
+    try:
+        xm = int(xm)
+        ym = int(ym)
+        isint = True
+    except:
+        isint = False
+    if isint == True:
+        track += 1
+        if thresholdRX <= xm:
+            print("LEFT SIDE", track)
+        elif thresholdLX >= xm:
+            print("RIGHT SIDE", track)
+        if thresholdUY > ym:  # errors on all the y coord stuff, need to figure out which corner the y coordinate is derived from
+            print("UP SIDE", track)
+            pass
+        elif thresholdBY < ym:
+            print("DOWN SIDE", track)
+            pass
 
-        if thresholdRX <= x:
-            tello.rotate_clockwise(10)
-            print("ROTATE RIGHT")
-        elif thresholdLX >= x:
-            tello.rotate_counter_clockwise(10)
-            print("ROTATE LEFT")
     else:
         pass
-def FindAruco(imag,markerSize=4,total_markers=1000,draw=True):
-    global gray,key,arucoDict,arucoParam,corners,ids
-    gray=cv2.cvtColor(imag,cv2.COLOR_BGR2GRAY)
-    key = getattr(cv2.aruco,f'DICT_{markerSize}X{markerSize}_{total_markers}')
-    arucoDict= cv2.aruco.getPredefinedDictionary(key)
-    arucoParam = cv2.aruco.DetectorParameters()
-    corners,ids,_=cv2.aruco.detectMarkers(gray,arucoDict,parameters=arucoParam)
-    #print(corners)
+def FindAruco(imag):
+    global gray, x, y, w, h, xm, ym, track
 
-    CoordMath(corners)
-    if draw:
-        DrawAruco(img,corners,ids)
-   #The coord stuff with the aruco
+    gray = cv2.cvtColor(imag, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(5, 5))
+    for (x, y, w, h) in faces:
+        cv2.rectangle(imag, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        try:
+            if faces is not None:
+                # print(x,y)
+                xc = x
+                yc = y
+                wid = w / 2
+                hei = h / 2
+                xm = xc + wid
+                ym = yc + hei
+                DroneController()
+
+            pass
+
+        except NameError:
+            pass
+    track = 0
 tello.takeoff()
 
 while True:
@@ -111,7 +137,6 @@ while True:
         img = cam
     FindAruco(img)
     #prints acceleration
-    DroneController()
     asyncio.run(AntiIdle())
 
 
