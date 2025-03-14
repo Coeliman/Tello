46f023eaecc4f52c13c66fe80b0d0541e5a1dcc2
Elijah Goris <coeliman24@gmail.com>
2025-03-14 09:50:59 -0600

    Delay for frames to limit latency (hopefully)

--- Face Image Creator.py
+++ Face Image Creator.py
@@ -15,8 +15,8 @@ while True:
     gray= cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=5,minSize=(100,100))
     for (x,y,w,h) in faces:
-        count=count+1
-        cv2.imwrite(r"C:\Users\Elijah Goris\Documents\Tello\Datasets/User."+str(id)+"."+str(count)+".jpg", gray[y:y+h, x:x+w])
+        count=count+1 #DO NOT UPLOAD MY ACTUAL USER TO GIT
+        cv2.imwrite(r"C:\Users\USER | DO NOT UPLOAD TO GET\Documents\Tello\Datasets/User."+str(id)+"."+str(count)+".jpg", gray[y:y+h, x:x+w])
         cv2.rectangle(frame, (x,y), (x+w, y+h), (50,50,255), 1)
 
     cv2.imshow("Frame",frame)
